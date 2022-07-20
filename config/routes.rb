0f4b2c446c22d37a07f50342c0bcbc883d9c5ac9
uni910 <megumioda27@gmail.com>
2022-07-20 21:37:26 +0900

    コンフリクト修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
-  get 'messages/index'
-  root to: "messages#index"
-end
-=======
   devise_for :users
   root to: "messages#index"
   resources :users, only: [:edit, :update]
 end
->>>>>>> Stashed changes
