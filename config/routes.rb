f3cb208ab3273af0562ae4a9233a2f73c66fa11a
uni910 <megumioda27@gmail.com>
2022-07-20 17:37:08 +0900

    プルリク

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   get 'messages/index'
   root to: "messages#index"
 end
+=======
+  devise_for :users
+  root to: "messages#index"
+  resources :users, only: [:edit, :update]
+end
+>>>>>>> Stashed changes
